9557b64a9b553e1a78307632efd4b9363adceb13
伊藤菜々子 <nanako.i.t.o27@gmail.com>
2018-06-13 21:09:16 +0900

    Java メソッド　引数・戻り値2課題再々提出します。

---
+++ CampChallenge_java/011.メソッド/TestData8.2java.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author itounanako
+ */
+public class TestData8 extends HttpServlet {
+   //配列を返すメソッド 引数は文字列型
+    String[][] profile() {
+        
+        String[][]a=new String[3][3];
+        
+        a[0][0]="ID:1111";
+        a[0][1]="2018/6/8";
+        a[0][2]="東京都九段下";
+        
+        a[1][0]="ID:2222";
+        a[1][1]="2018/6/9";
+        a[1][2]="東京都";
+        
+        a[2][0]="ID:3333";
+        a[2][1]="2018/6/10";
+        a[2][2]=null;
+        
+        //戻り値//
+        return a;
+        
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String[][] a=profile();
+       
+        for(int i=0;i<a.length;i++){
+        for(int j=0;j<a.length;j++){    
+        //continue;で画面表示をスキップ
+        if(a[i][j]==null){
+       continue;    
+        }
+        out.println(a[i][j]);       
+        
+        }
+        
+    }
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
+
